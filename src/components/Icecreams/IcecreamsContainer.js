1b7651b915decadaec3cdfe52838922db85fc0f3
rajendra7117 <rajendrabvm0007@gmail.com>
2022-09-06 20:36:49 +0530

    icecreame-feature added

--- src/components/Icecreams/IcecreamsContainer.js
+++ src/components/Icecreams/IcecreamsContainer.js
@@ -1,15 +1,15 @@
 import React, {useState} from 'react'
 import { useSelector,useDispatch  } from 'react-redux/'
-import { buyIcecream } from '../../redux/icecreams/icecreamActions'
+import { icecreameSliceActions } from '../../redux-toolkit/icecreams/icecreameSlice'
 const IcecreamsContainer = () => {
     const [icecreams, setIcecreams] = useState(1)
-    const numberOfIcecreams = useSelector(state => state.icecreame.numberOfIcecreams)
+    const numberOfIcecreams = useSelector(state => state.icecreames.icecreams)
     const dispatch = useDispatch()
   return (
     <div>
         <h1>Number of icecreams - {numberOfIcecreams} </h1>
         <input type="number" min={1} max={5} onChange={(e) => setIcecreams(e.target.value)}  value={icecreams}/>
-        <button onClick={() => dispatch(buyIcecream({icecreams: icecreams}))}>buy {icecreams} icecreame</button>
+        <button onClick={() => dispatch(icecreameSliceActions.buyIcecreame(icecreams))}>buy {icecreams} icecreame</button>
     </div>
   )
 }

c7a891fbebdf0ede0d9f38dc2a19e623398f1b26
rajendra7117 <rajendrabvm0007@gmail.com>
2022-09-06 20:16:01 +0530

    cake feature added

--- src/redux-toolkit/store.js
+++ src/redux-toolkit/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersSlice from "./users/usersSlice";
+import cakeSlice from "./cakes/cakeSlice";
 
 const store = configureStore({
     reducer:{
-        users: usersSlice.reducer
+        users: usersSlice.reducer,
+        cakes: cakeSlice.reducer
     }
 })
 
